0077876feae383090975300a3a9dbe1915e441ad
Abigoli J <debo.abi23@gmail.com>
2021-11-20 00:12:45 -0300

    Trabajos

---
+++ Proyecto front 1/tp.js
@@ -0,0 +1,12 @@
+let temp = 25;
+let piso  =10;
+let lloviendo = true;
+let hora_actual=2;
+let hora_cierre=17;
+let cantidadQueso=100;
+let deuda_monica=2000;
+
+//nuestro ejemplo
+
+let materia = "Programacion Imperativa"
+
